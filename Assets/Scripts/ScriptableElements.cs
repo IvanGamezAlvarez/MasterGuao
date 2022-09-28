c7752e752097a2e7d67e0ea9f2b9f6b77c317b82
IvanGamez <ivan_masteer@hotmail.com>
2022-09-28 12:45:08 -0500

    NewChanges

--- Assets/Scripts/ScriptableElements.cs
+++ Assets/Scripts/ScriptableElements.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "VariablesParaCocina", menuName ="VariablesParaLosElementosDeCocina")]
 public class ScriptableElements : ScriptableObject
 {
-
+    public bool tieneElVegetalCorrecta;
+    public string[] vegetalesAceptados;
 }
